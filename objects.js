ad2cceeffba50bc141132c63a9cc4d0c6883cc21
Lovinaj <85228654+Lovinaj@users.noreply.github.com>
2021-06-09 11:29:50 +0100

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,20 @@
+// function greeting(name) {   //Callback Function
+//     alert('Hello' +  name);
+// }
+
+// function userInput(today) {
+//     let myName = "Pete";
+//     today(myName);
+// }
+// userInput(greeting);
+
+// let sum = (a,b) => { //Arrow Function
+//     return a + b;
+// }
+// alert(sum(45, 2))
+
+// For loop
+let arr = ["Apple", "Orange", "Pear"];
+for (let i = 0; i < arr.length; i++);
+
+
